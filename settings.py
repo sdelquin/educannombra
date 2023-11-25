2154f03cee445bf6f2fee4b97ed3cbf23f0e1b75
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-25 19:45:36 +0000

    First load of code

---
+++ settings.py
@@ -0,0 +1,5 @@
+DESIGNATION_URLS = {
+    'Maestros y maestras de la provincia de Las Palmas': 'https://www.gobiernodecanarias.org/educacion/DGPer/NombraDiarios/Docs/NDPLP{date}.PDF',
+    'Maestros y maestras de la provincia de Santa Cruz de Tenerife': 'https://www.gobiernodecanarias.org/educacion/DGPer/NombraDiarios/Docs/NDPTF{date}.PDF',
+    'Profesorado de otros cuerpos: ambas provincias': 'https://www.gobiernodecanarias.org/educacion/DGPer/NombraDiarios/Docs/NDS{date}.PDF',
+}
